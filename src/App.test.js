3dc962fad45e6c2c422a18c96587babf8ed000dd
Felix Zhang <63573266+zl342830224@users.noreply.github.com>
2020-09-29 16:01:41 +0800

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,7 @@ import App from './App';
 
 it('renders without crashing', () => {
         const div = document.createElement('div');
-        ReactDom.render((
+        ReactDOM.render((
                  <StaticRouter>
                         <App />
                  </StaticRouter>
